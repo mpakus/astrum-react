2ecfc5c97493172634f92659e01b2b450184fd20
mpakus <mrak69@gmail.com>
2017-05-13 22:13:33 +0700

    Redux added
    yarn add redux react-redux

--- client-redux/src/index.jsx
+++ client-redux/src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import App from './components/app/index';
 import './index.css';
+// import Provider from "react-redux/src/components/Provider";
+
+const store = createStore( ()=> {}, {} );
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store} >
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
