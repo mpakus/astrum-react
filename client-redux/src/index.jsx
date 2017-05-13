6fb4f69f02e13a30a1efb881c35349a9922c3a89
mpakus <mrak69@gmail.com>
2017-05-13 21:37:20 +0700

    Prepared base for Lesson #1 React + Redux

---
+++ client-redux/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/index';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
