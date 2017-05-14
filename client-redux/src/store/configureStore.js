1f9da508668149d4a4075cbcdb7d807e1093da45
mpakus <mrak69@gmail.com>
2017-05-14 22:22:35 +0700

    redux-logger

--- client-redux/src/store/configureStore.js
+++ client-redux/src/store/configureStore.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import { createLogger } from 'redux-logger';
 
 export default function configureStore(initialState){
-  return createStore( rootReducer, initialState );
+  const logger = createLogger();
+  return createStore( rootReducer, initialState, applyMiddleware(logger) );
 }
